b43ea71d60adb3c5bde2ce2ca53a36e0043d0d39
asaPhone2 <amge.hero23@gmail.com>
2021-01-23 12:34:19 +0900

    ルーティング・コントローラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :comments
+  
+  validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze } do
+    validates :password
+  end
+
+  with_options presence: true do
+    validates :nickname
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+
+  end
 end
